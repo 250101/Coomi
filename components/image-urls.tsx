86094f9d2d6a0869cfad14f66bacbe259140fbf7
250101 <martin.alejandro.moore@gmail.com>
2025-03-29 18:01:40 -0300

    Images

--- components/image-urls.tsx
+++ components/image-urls.tsx
@@ -4,59 +4,59 @@
 export const imageUrls = [
   {
     name: "1.jpeg",
-    url: "/images/1.jpeg",
+    url: "/Images/1.jpeg",
   },
   {
     name: "2.jpeg",
-    url: "/images/2.jpeg",
+    url: "/Images/2.jpeg",
   },
   {
     name: "3.jpeg",
-    url: "/images/3.jpeg",
+    url: "/Images/3.jpeg",
   },
   {
     name: "4.jpeg",
-    url: "/images/4.jpeg",
+    url: "/Images/4.jpeg",
   },
   {
     name: "5.jpeg",
-    url: "/images/5.jpeg",
+    url: "/Images/5.jpeg",
   },
   {
     name: "6.jpeg",
-    url: "/images/6.jpeg",
+    url: "/Images/6.jpeg",
   },
   {
     name: "7.jpeg",
-    url: "/images/7.jpeg",
+    url: "/Images/7.jpeg",
   },
   {
     name: "8.jpeg",
-    url: "/images/8.jpeg",
+    url: "/Images/8.jpeg",
   },
   {
     name: "9.jpeg",
-    url: "/images/9.jpeg",
+    url: "/Images/9.jpeg",
   },
   {
     name: "10.jpeg",
-    url: "/images/10.jpeg",
+    url: "/Images/10.jpeg",
   },
   {
     name: "11.jpeg",
-    url: "/images/11.jpeg",
+    url: "/Images/11.jpeg",
   },
   {
     name: "12.jpeg",
-    url: "/images/12.jpeg",
+    url: "/Images/12.jpeg",
   },
   {
     name: "13.jpeg",
-    url: "/images/13.jpeg",
+    url: "/Images/13.jpeg",
   },
   {
     name: "14.jpeg",
-    url: "/images/14.jpeg",
+    url: "/Images/14.jpeg",
   },
 ]
 
