d00cb3f3b01a6bb73a8a7feba6d73721c6da2003
250101 <martin.alejandro.moore@gmail.com>
2025-03-29 15:55:55 -0300

    Actualizacion de textos

--- components/image-urls.tsx
+++ components/image-urls.tsx
@@ -4,59 +4,59 @@
 export const imageUrls = [
   {
     name: "1.jpeg",
-    url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Crema%20de%20Boniato-PQv0FBkoMU5jeIsLvu7BISSeDNW8wz.jpeg",
+    url: "./image/1.jpeg",
   },
   {
     name: "2.jpeg",
-    url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Manteca%20Miso-Z5O1hlvIE4HXWsmxs378eSwqGc2W9I.jpeg",
+    url: "./image/2.jpeg",
   },
   {
     name: "3.jpeg",
-    url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Crema%20de%20hongos%20y%20cebolla-2Y3VaLEKkZBARIIkneu5V6NUX8En2P.jpeg",
+    url: "./image/3.jpeg",
   },
   {
     name: "4.jpeg",
-    url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Croquetas%20de%20Risotto-dPueFj3JZ02YMOvyNIbZeU2pKjqDfi.jpeg",
+    url: "./image/4.jpeg",
   },
   {
     name: "5.jpeg",
-    url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Croquetas%20de%20Osobuco-LHdpAWhTiCmTSZ0fSn2IKXZT5I85QV.jpeg",
+    url: "./image/5.jpeg",
   },
   {
     name: "6.jpeg",
-    url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Empanadas-ZObhNmZ4Upc5Ak8GHWzsnWbVRNfQI0.jpeg",
+    url: "./image/6.jpeg",
   },
   {
     name: "7.jpeg",
-    url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Katsudon-rE1kYqgUw8XbQrEWC6ftuPODtt3rsY.jpeg",
+    url: "./image/7.jpeg",
   },
   {
     name: "8.jpeg",
-    url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Katsu%20Curry-n6ybmC9RgkVn6i5WkKEpvCNzCXG7KO.jpeg",
+    url: "./image/8.jpeg",
   },
   {
     name: "9.jpeg",
-    url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Risotto%20Curry-n8bSxoZNxHKFOn2B5HL9U818cbvpK3.jpeg",
+    url: "./image/9.jpeg",
   },
   {
     name: "10.jpeg",
-    url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Lomo%20con%20un%20mar%20de%20boniato%20y%20papas%20rejillas-PLcJWJsia3G1cg1hXS4eUfmfhJfmwJ.jpeg",
+    url: "./image/10.jpeg",
   },
   {
     name: "11.jpeg",
-    url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Tortilla-718cBk7NzGSTZvHFOawJGDkUB2BxdL.jpeg",
+    url: "./image/11.jpeg",
   },
   {
     name: "12.jpeg",
-    url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Tiramisu-mXuWQnPj3sNgDwlJUiiOvBj0aoO16h.jpeg",
+    url: "./image/12.jpeg",
   },
   {
     name: "13.jpeg",
-    url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Rolls%20de%20Canela-vWJYper9MDQZiemjN81oeo9mNrazRV.jpeg",
+    url: "./image/13.jpeg",
   },
   {
     name: "14.jpeg",
-    url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Cookie-I5c3MGMLMxGIvPCrWtXPnUeCgIwpZQ.jpeg",
+    url: "./image/14.jpeg",
   },
 ]
 
